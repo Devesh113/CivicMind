ffe5e9182a6a8e032f54ba145d45523054ee1f49
Himanshu Dubey <himanshudubey2401@gmail.com>
2025-11-27 00:05:17 +0530

    fix#2

--- index-dev.ts
+++ index-dev.ts
@@ -6,7 +6,7 @@ import { nanoid } from "nanoid";
 import { type Express } from "express";
 import { createServer as createViteServer, createLogger } from "vite";
 
-import viteConfig from "../vite.config";
+import viteConfig from "./vite.config";
 import runApp from "./app";
 
 export async function setupVite(app: Express, server: Server) {
@@ -38,8 +38,6 @@ export async function setupVite(app: Express, server: Server) {
     try {
       const clientTemplate = path.resolve(
         import.meta.dirname,
-        "..",
-        "client",
         "index.html",
       );
 
